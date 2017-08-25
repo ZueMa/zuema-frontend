c61ca3a89f28167dfa7a7d17c3a307ba37d03446
mild nchrt <mildnchrt@milds-MacBook-Pro.local>
2017-08-25 23:03:22 +0700

    Create head table and button

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react'
+import '../stylesheets/cart.css'
 
 class Cart extends Component {
   render() {
     return(
-      <div>
-        CART
+      <div className="container cart">
+        <div className="container head-cart-page">
+          <span className="page-title">YOUR SHOPPING CART</span>
+          <button className="checkout-cart-btn">CHECKOUT</button>
+          <button className="edit-cart-btn">EDIT</button>
+        </div>
+        
+        <table className="table-cart">
+          <thead>
+            <th>PRODUCT NAME</th>
+            <th>PRICE</th>
+            <th>QUANTITY</th>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td className="cart-product-name">
+                <span>Product</span><br/>
+                <p>short description</p>
+              </td>
+              <td>130.00</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td className="cart-product-name">
+                <span>Product</span><br/>
+                <p>short description</p>
+              </td>
+              <td>130.00</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td className="cart-product-name">
+                <span>Product</span><br/>
+                <p>short description</p>
+              </td>
+              <td>130.00</td>
+              <td>1</td>
+            </tr>
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td></td>
+              <td>Total</td>
+              <td>1000.00</td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     )
   }
