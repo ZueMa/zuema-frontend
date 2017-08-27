9453632232642de4abfe2250852cfb0c71fa6491
Nune <littlenune@gmail.com>
2017-08-27 10:48:06 +0700

    add profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import '../stylesheets/profile.css'
 
 class Profile extends Component {
+  constructor(props){
+    super(props;
+
+    this.state = { profiles:[] };
+    axios.get('https://private-00f7e-zuema.apiary-mock.com/buyers/me').then(profiles => {
+      this.setState({ profiles });
+    });
+  }
+
   render() {
     return(
       <div>
-        Profile
+        <h1>PROFILE</h1>
+        <p></p>
+        <p id="type">BUYER</p>
+        <p>USERNAME : {profiles[this.state.username}</p></p>
+        <p>FIRSTNAME : {profiles[this.state.firstname}</p>
+        <p>LASTNAME : {profiles[this.state.lastname}</p>
+        <p>ADDRESS : {profiles[this.state.address}</p>
       </div>
     )
   }
