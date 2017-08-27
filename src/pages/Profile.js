b92dbe93d16040a255b3a4b6a6e8b0bdd2bdf49f
Nune <littlenune@gmail.com>
2017-08-27 10:58:36 +0700

    get user's profile from api

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import '../stylesheets/profile.css'
+import axios from 'axios'
+
 
 class Profile extends Component {
   constructor(props){
-    super(props;
+    super(props);
 
     this.state = { profiles:[] };
-    axios.get('https://private-00f7e-zuema.apiary-mock.com/buyers/me').then(profiles => {
+    axios.get('https://private-00f7e-zuema.apiary-mock.com/buyers/me').then( res => {
+      const profiles = res.data;
       this.setState({ profiles });
     });
   }
@@ -17,10 +20,10 @@ class Profile extends Component {
         <h1>PROFILE</h1>
         <p></p>
         <p id="type">BUYER</p>
-        <p>USERNAME : {profiles[this.state.username}</p></p>
-        <p>FIRSTNAME : {profiles[this.state.firstname}</p>
-        <p>LASTNAME : {profiles[this.state.lastname}</p>
-        <p>ADDRESS : {profiles[this.state.address}</p>
+        <p>USERNAME : {this.state.profiles.username}</p>
+        <p>FIRSTNAME : {this.state.profiles.first_name}</p>
+        <p>LASTNAME : {this.state.profiles.last_name}</p>
+        <p>ADDRESS : {this.state.profiles.address}</p>
       </div>
     )
   }
