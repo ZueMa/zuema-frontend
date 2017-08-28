4918dab8df9e9a3365335ffecf7151b924c7d3db
Nune <littlenune@gmail.com>
2017-08-28 16:36:50 +0700

    change page style

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,13 +5,11 @@ import axios from 'axios'
 class Profile extends Component {
   constructor(props){
     super(props);
-
     this.state = { profiles:[] };
-   
   }
 
   componentWillMount(){
-    axios.get('https://private-00f7e-zuema.apiary-mock.com/buyers/me').then( res => {
+    axios.get('https://private-00f7e-zuema.apiary-mock.com/sellers/me').then( res => {
       const profiles = res.data;
       this.setState({ profiles });
     });
@@ -20,22 +18,33 @@ class Profile extends Component {
   render() {
     return(
       <div className="container-box">
-        <div className="text-head">PROFILE
-          <div className="line-rectangle"></div>
-          <p id="type">BUYER</p>
-          <div className="info">
-            <br/>
-            <p>USERNAME<span className="indent"></span>{this.state.profiles.username}</p>
-            <p>FIRSTNAME<span className="indent"></span>{this.state.profiles.first_name}</p>
-            <p>LASTNAME<span className="indent"></span>{this.state.profiles.last_name}</p>
-            <p>ADDRESS<span className="indent"></span>{this.state.profiles.address}</p>
-            <br/>
+      <div className="text-head">PROFILE
+        <div className="line-rectangle"></div>
+        <p id="type">SELLER</p>
+        <div className="info">
+          <div id="wrapper" className="input-form">
+            <div id="left-col">
+              <p>USERNAME</p>
+              <p>FIRSTNAME</p>
+              <p>LASTNAME</p>
+              <p>COMPANY NAME</p>
+              <p>INFORMATION</p>
+              <p>ADDRESS</p>
+            </div>
+            <div id="right-col">
+              <p>{this.state.profiles.username}</p>
+              <p>{this.state.profiles.first_name}</p>
+              <p>{this.state.profiles.last_name}</p>
+              <p>{this.state.profiles.company_name}</p>
+              <p>{this.state.profiles.description}</p>
+              <p>{this.state.profiles.address}</p>
+            </div>
           </div>
+          <br/>
         </div>
-
-
-        <a href={this.url} id="history">VIEW ORDER HISTORY</a>
       </div>
+        <a href={'http://localhost:3000/history'} id="history">VIEW ORDER HISTORY</a>  
+    </div>
     )
   }
 }
