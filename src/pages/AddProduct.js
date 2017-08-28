61f1487b90ef3c4b09b074c5c5b1d0a079c72406
Nune <littlenune@gmail.com>
2017-08-28 08:18:36 +0700

    change page style

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -6,23 +6,41 @@ class AddProduct extends Component {
   render() {
     return(
       <div className="container-box">
-
         <div className="text-head">ADD YOUR PRODUCT
           <div className="line-rectangle"></div>
           <p id="type">SELLER</p>
         </div>
 
-        <div className="input-form">
-          <p>PRODUCT NAME*</p>
-          <input></input>
-        </div>
-        <div className="input-form">
-          <p>SHORT DESCRIPTION*</p>
-          <input></input>
-        </div>
-        <div className="input-form">
-          <p>FULL DESCRIPTION*</p>
-          <input></input>
+        <div id="wrapper" className="input-form">
+            <div id="left-col">
+              <p>PRODUCT NAME*</p>
+              <input size="50"></input>
+              <br/>
+              <p>SHORT DESCRIPTION*</p>
+              <input size="50"></input>
+              <br/>
+              <p>FULL DESCRIPTION*</p>
+              <textarea id="input-text" cols="51" rows="6"></textarea>
+            </div>
+            <div id="right-col">
+              <p>CHOOSE CATEGORY*</p>
+              
+              <select>
+                <option>Clothes</option>
+                <option>SPORTS</option>
+                <option>Clothes</option>
+                <option>Clothes</option>
+              </select>
+
+              <p>PRODUCT PRICE*</p>
+              <input size="10"></input>
+              <p>PRODUCT QTY*</p>
+              <input size="10"></input>
+              <p>PRODUCT IMAGE*</p>
+              <a href={this.url} id="history">CHOOSE IMAGE</a>
+              <br/>
+              <a href={this.url} id="history">ADD PRODUCT</a>
+            </div>
         </div>
 
       </div>
