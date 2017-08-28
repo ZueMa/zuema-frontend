3d027ad6269e1a543db19adf8cb3d0a7a703af22
babestvl <babestvl@gmail.com>
2017-08-29 00:52:05 +0700

    Add seller component & pre onclick function

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -5,7 +5,19 @@ import '../stylesheets/product.css'
 class Product extends Component {
   constructor(props) {
     super(props)
-    this.role = 'buyer'
+    this.role = 'seller'
+  }
+
+  handleAddCart = (e) => {
+
+  }
+
+  handleEdit = (e) => {
+
+  }
+
+  handleRemove = (e) => {
+
   }
 
   render() {
@@ -27,13 +39,36 @@ class Product extends Component {
                 <div className="product-price">{products[this.props.match.params.id].price} BAHT</div>
                 <p className="product-stock">Only {products[this.props.match.params.id].stock} left in stock</p>
               </div>
-              <button className="btn btn-add-cart">ADD TO CART</button>
+              <button className="btn btn-add-cart" onClick={this.handleAddCart}>ADD TO CART</button>
             </div>
           </div>
         </div>
       ) 
     } else if (this.role === 'seller') {
-
+      component = (
+        <div className="container-fluid product-height">
+          <div className="row product-style-div">
+            <div className="col-md-6 no-padding image-side">
+              <div className="product-full-image"></div>
+            </div>
+            <div className="col-md-6 text-side">
+              <div className="text-head">
+                {products[this.props.match.params.id].name}
+                <div className="line-rectangle"></div>
+              </div>
+              <div className="product-full-info">
+                <p className="product-text">{products[this.props.match.params.id].detail}</p>
+                <div className="product-price">{products[this.props.match.params.id].price} BAHT</div>
+                <p className="product-stock">Only {products[this.props.match.params.id].stock} left in stock</p>
+              </div>
+              <div className="button-container">
+                <button className="btn btn-edit" onClick={this.handleEdit}>EDIT</button>
+                <button className="btn btn-remove" onClick={this.handleRemove}>REMOVE</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
     }
     return(
       <div className="product">
