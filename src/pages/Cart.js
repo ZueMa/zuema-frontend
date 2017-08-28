352d0df95f0bff643b210092d083c024e682a232
Mildnchrt <Nutcharueta.s@ku.th>
2017-08-28 21:58:15 +0700

    Link edit button to Edit Cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import '../stylesheets/cart.css'
+import { Link } from 'react-router-dom'
 
 class Cart extends Component {
   render() {
@@ -7,25 +8,27 @@ class Cart extends Component {
       <div className="container cart">
         <div className="container head-cart-page">
           <span className="page-title">YOUR SHOPPING CART</span>
-          <button className="checkout-cart-btn">CHECKOUT</button>
-          <button className="edit-cart-btn">EDIT</button>
+          <button className="btn checkout-cart-btn">CHECKOUT</button>
+          <button className="btn edit-cart-btn">EDIT<Link to={'/cart/edit'}></Link>
+          </button>
         </div>
         
         <table className="table-cart">
           <thead>
+            <th></th>
             <th>PRODUCT NAME</th>
-            <th>PRICE</th>
-            <th>QUANTITY</th>
+            <th className="topic-center">PRICE</th>
+            <th className="topic-center">QUANTITY</th>
           </thead>
 
           <tbody>
             <tr>
-              <td className="cart-product-list">
+              <td>
                 <img className="cart-product-img"></img>
-                <div className="cart-product-detail">
-                  <span className="order-name">PRODUCT NAME</span><br/>
-                  <p className="order-description">Short description</p>
-                </div>
+              </td>
+              <td className="cart-product-list">
+                <span className="order-name">PRODUCT NAME</span><br/>
+                <p className="order-description">Short description</p>
               </td>
               <td>130.00</td>
               <td>1</td>
@@ -34,6 +37,7 @@ class Cart extends Component {
 
           <tfoot>
             <tr>
+              <td></td>
               <td></td>
               <td>TOTAL</td>
               <td>1000.00</td>
