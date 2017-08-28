7f36c501c923d6117f06228dbf73c58e61124d9c
babestvl <babestvl@gmail.com>
2017-08-28 14:34:38 +0700

    Commit index page

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -23,9 +23,19 @@ class Store extends Component {
   render() {
     return(
       <div className="Store">
+        <div className="TopBar" style={{textAlign: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+          <h3 style={{margin: '0'}}>
+            <i className="fa fa-search" style={{marginRight: '10px'}}></i>
+            SEARCH
+          </h3>
+        </div>
         <Categories/>
         <div style={{color: 'grey', textAlign: 'center'}}>
-          <h2>- ALL PRODUCT -</h2> 
+          <div style={{display: 'inline-flex', alignItems: 'center', marginTop: '15px'}}>
+            <hr className="line"/>
+            <h2 style={{marginLeft: '10px', marginRight: '10px'}}>ALL PRODUCT</h2> 
+            <hr className="line"/>
+          </div>
         </div>
         {products.map((itm, id) => {
           return <ProductCard name={itm.name} detail={itm.detail} id={id} price={itm.price} key={id}/>
