8624aa5c75b5286ad9f35b5677515e811d023d47
Nune <littlenune@gmail.com>
2017-08-29 09:35:04 +0700

    separate buyer and seller

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,47 +5,66 @@ import axios from 'axios'
 class Profile extends Component {
   constructor(props){
     super(props);
-    this.state = { profiles:[] };
+    this.state = { profiles:[],type: 'BUYER' };
   }
 
   componentWillMount(){
-    axios.get('https://private-00f7e-zuema.apiary-mock.com/sellers/me').then( res => {
-      const profiles = res.data;
-      this.setState({ profiles });
-    });
+    if (this.state.type === 'SELLER'){
+      axios.get('https://private-00f7e-zuema.apiary-mock.com/sellers/me').then( res => {
+        const profiles = res.data;
+        this.setState({ profiles });
+      });
+    }
+    else if (this.state.type === 'BUYER'){
+      axios.get('https://private-00f7e-zuema.apiary-mock.com/buyers/me').then( res => {
+        const profiles = res.data;
+        this.setState({ profiles });
+      });
+    }
   }
 
   render() {
-    return(
-      <div className="container-box">
-      <div className="text-head">PROFILE
-        <div className="line-rectangle"></div>
-        <p id="type">SELLER</p>
-        <div className="info">
-          <div id="wrapper" className="input-form">
-            <div id="left-col">
-              <p>USERNAME</p>
-              <p>FIRSTNAME</p>
-              <p>LASTNAME</p>
-              <p>COMPANY NAME</p>
-              <p>INFORMATION</p>
-              <p>ADDRESS</p>
-            </div>
-            <div id="right-col">
-              <p>{this.state.profiles.username}</p>
-              <p>{this.state.profiles.first_name}</p>
-              <p>{this.state.profiles.last_name}</p>
-              <p>{this.state.profiles.company_name}</p>
-              <p>{this.state.profiles.description}</p>
-              <p>{this.state.profiles.address}</p>
+    if (this.state.type === 'BUYER'){
+      return(
+        <div className="container-box">
+        <div className="text-head">PROFILE
+          <div className="line-rectangle"></div>
+          <p id="type">BUYER</p>
+          <div className="info">
+            <div id="wrapper" className="input-form">
+                <p>USERNAME       {this.state.profiles.username}</p>
+                <p>FIRSTNAME      {this.state.profiles.first_name}</p>
+                <p>LASTNAME       {this.state.profiles.last_name}</p>
+                <p>ADDRESS        {this.state.profiles.address}</p>
+              <a href={'http://localhost:3000/history'} id="history">VIEW ORDER HISTORY</a>  
             </div>
           </div>
-          <br/>
         </div>
       </div>
-        <a href={'http://localhost:3000/history'} id="history">VIEW ORDER HISTORY</a>  
-    </div>
-    )
+      )
+    }
+    else if (this.state.type === 'SELLER'){
+      return(
+        <div className="container-box">
+        <div className="text-head">PROFILE
+          <div className="line-rectangle"></div>
+          <p id="type">SELLER</p>
+          <div className="info">
+            <div id="wrapper" className="input-form">
+                <p>USERNAME       {this.state.profiles.username}</p>
+                <p>FIRSTNAME      {this.state.profiles.first_name}</p>
+                <p>LASTNAME       {this.state.profiles.last_name}</p>
+                <p>COMPANY NAME   {this.state.profiles.company_name}</p>
+                <p>INFORMATION    {this.state.profiles.description}</p>
+                <p>ADDRESS        {this.state.profiles.address}</p>
+              <a href={'http://localhost:3000/history'} id="history">VIEW ORDER HISTORY</a>  
+            </div>
+          </div>
+        </div>
+      </div>
+      )
+    }
+   
   }
 }
 
