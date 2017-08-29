b91b8fdf7dd2fa3a10c52c5ee390e05622906a12
Nune <littlenune@gmail.com>
2017-08-29 10:24:08 +0700

    adjust indent

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -26,45 +26,49 @@ class Profile extends Component {
   render() {
     if (this.state.type === 'BUYER'){
       return(
-        <div className="container-box">
-        <div className="text-head">PROFILE
-          <div className="line-rectangle"></div>
-          <p id="type">BUYER</p>
-          <div className="info">
-            <div id="wrapper" className="input-form">
-                <p>USERNAME       {this.state.profiles.username}</p>
-                <p>FIRSTNAME      {this.state.profiles.first_name}</p>
-                <p>LASTNAME       {this.state.profiles.last_name}</p>
-                <p>ADDRESS        {this.state.profiles.address}</p>
-              <a href={'http://localhost:3000/history'} id="history">VIEW ORDER HISTORY</a>  
+        <div class="container-fluid">
+          <div className="container-box">
+            <div className="text-head">PROFILE
+              <div className="line-rectangle"></div>
+              <p id="type">BUYER</p>
+              <div className="info">
+                <div id="wrapper" className="input-form">
+                    <p>USERNAME<h3 >{this.state.profiles.username}</h3></p>
+                    <p>FIRSTNAME<h3>{this.state.profiles.first_name}</h3></p>
+                    <p>LASTNAME<h3 id="lastname">{this.state.profiles.last_name}</h3></p>
+                    <p>ADDRESS<h3 id="address">{this.state.profiles.address}</h3></p>
+                  <a href={'http://localhost:3000/history'} id="history">VIEW ORDER HISTORY</a>  
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       )
     }
     else if (this.state.type === 'SELLER'){
       return(
-        <div className="container-box">
-        <div className="text-head">PROFILE
-          <div className="line-rectangle"></div>
-          <p id="type">SELLER</p>
-          <div className="info">
-            <div id="wrapper" className="input-form">
-                <p>USERNAME       {this.state.profiles.username}</p>
-                <p>FIRSTNAME      {this.state.profiles.first_name}</p>
-                <p>LASTNAME       {this.state.profiles.last_name}</p>
-                <p>COMPANY NAME   {this.state.profiles.company_name}</p>
-                <p>INFORMATION    {this.state.profiles.description}</p>
-                <p>ADDRESS        {this.state.profiles.address}</p>
-              <a href={'http://localhost:3000/history'} id="history">VIEW ORDER HISTORY</a>  
+        <div class="container-fluid">
+          <div className="container-box">
+          <div className="text-head">PROFILE
+            <div className="line-rectangle"></div>
+            <p id="type">SELLER</p>
+            <div className="info">
+              <div id="wrapper" className="input-form">
+                <p>USERNAME<h3>{this.state.profiles.username}</h3></p>
+                <p>FIRSTNAME<h3>{this.state.profiles.first_name}</h3></p>
+                <p>LASTNAME<h3 id="lastname">{this.state.profiles.last_name}</h3></p>
+                <p>COMPANY NAME<h3 id="company">{this.state.profiles.company_name}</h3></p>
+                <p>INFORMATION<h3 id="des">{this.state.profiles.description}</h3></p>
+                <p>ADDRESS<h3 id="address">{this.state.profiles.address}</h3></p>
+                <a href={'http://localhost:3000/history'} id="history">VIEW ORDER HISTORY</a>  
+              </div>
             </div>
           </div>
         </div>
       </div>
       )
     }
-   
+
   }
 }
 
