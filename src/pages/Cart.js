732a843211a278111754978ad95a096d70ab199e
Mildnchrt <Nutcharueta.s@ku.th>
2017-08-31 17:28:13 +0700

    Use table shared component in cart and cartEdit page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,37 +1,40 @@
 import React, { Component } from 'react'
 import '../stylesheets/cart.css'
 import { Link } from 'react-router-dom'
+import ProductTableListCart from '../components/ProductTableListCart'
 
 class Cart extends Component {
   render() {
     return(
       <div className="container cart">
-        <div className="container head-cart-page">
-          <span className="page-title">YOUR SHOPPING CART</span>
-          <button className="btn checkout-cart-btn">CHECKOUT</button>
-          <Link to={'/cartedit'}><button className="btn edit-cart-btn">EDIT</button></Link>
+        <div className="head-cart-page row">
+          <div className="col-md-6">
+            <div className="head">YOUR SHOPPING CART</div>
+            <div className="color_line_head"></div><br />
+          </div>
+          <div className="col-md-6">
+            <button className="btn checkout-cart-btn">CHECKOUT</button>
+            <Link to={'/cartedit'}><button className="btn edit-cart-btn">EDIT</button></Link>
+          </div>
         </div>
         
         <table className="table table-hover">
           <thead>
-            <th></th>
-            <th>PRODUCT NAME</th>
-            <th className="topic-center">PRICE</th>
-            <th className="topic-center">QUANTITY</th>
+            <tr>
+              <th></th>
+              <th className="head_table_title">PRODUCT NAME</th>
+              <th className="head_table_title_center">PRICE</th>
+              <th className="head_table_title_center">QUILITY</th>
+            </tr>
           </thead>
 
           <tbody>
-            <tr>
-              <td>
-                <img className="cart-product-img"></img>
-              </td>
-              <td className="cart-product-list">
-                <span className="order-name">PRODUCT NAME</span><br/>
-                <p className="order-description">Short description</p>
-              </td>
-              <td>130.00</td>
-              <td>1</td>
-            </tr>
+            <ProductTableListCart 
+            name="PRODUCT NAME" 
+            short_descrition="Short description"
+            price="100"
+            image="logo.png"
+            quantity="1"/>
           </tbody>
 
           <tfoot>
