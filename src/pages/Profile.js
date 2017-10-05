25d72403d2d482df03e149f08d7573337f0933cd
babestvl <babestvl@gmail.com>
2017-10-05 16:12:25 +0700

    Add margin bottom profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -44,14 +44,14 @@ class Profile extends Component {
               <p id="type">BUYER</p>
               <div className="info">
               <dl id="dl-horizontal">
-                     <dt>USERNAME</dt>
-                     <dd>{this.state.profiles.username}</dd>
-                     <dt>FIRSTNAME</dt>
-                     <dd>{this.state.profiles.first_name}</dd>
-                     <dt>LASTNAME</dt>
-                     <dd>{this.state.profiles.last_name}</dd>         
-                     <dt>ADDRESS</dt>
-                     <dd>{this.state.profiles.address}</dd>
+                <dt>USERNAME</dt>
+                <dd>{this.state.profiles.username}</dd>
+                <dt>FIRSTNAME</dt>
+                <dd>{this.state.profiles.first_name}</dd>
+                <dt>LASTNAME</dt>
+                <dd>{this.state.profiles.last_name}</dd>         
+                <dt>ADDRESS</dt>
+                <dd>{this.state.profiles.address}</dd>
               </dl>
             <br/>
             <Link to={'purchasehistorybuyer'} id="history">VIEW PURCHASE HISTORY</Link>
@@ -66,30 +66,29 @@ class Profile extends Component {
           <div className="text-head">PROFILE
             <div className="line-rectangle"></div>
             <p id="type">SELLER</p>
-                <div className="info">
-            <div className="panel panel-danger">
-                    <div className="panel-body">
-                    <dl className="dl-horizontal">
-                      <dt>USERNAME</dt>
-                      <dd>{this.state.profiles.username}</dd>
-                      <dt>FIRSTNAME</dt>
-                      <dd>{this.state.profiles.first_name}</dd>
-                      <dt>LASTNAME</dt>
-                      <dd>{this.state.profiles.last_name}</dd>
-                      <dt>INFORMATION</dt>
-                      <dd>{this.state.profiles.description}</dd>
-                      <dt>COMPANY NAME</dt>
-                      <dd>{this.state.profiles.company_name}</dd>
-                      <dt>ADDRESS</dt>
-                      <dd>{this.state.profiles.address}</dd>
-                    </dl>
-                    </div>
+            <div className="info">
+              <div className="panel panel-danger">
+                <div className="panel-body">
+                  <dl className="dl-horizontal">
+                    <dt>USERNAME</dt>
+                    <dd>{this.state.profiles.username}</dd>
+                    <dt>FIRSTNAME</dt>
+                    <dd>{this.state.profiles.first_name}</dd>
+                    <dt>LASTNAME</dt>
+                    <dd>{this.state.profiles.last_name}</dd>
+                    <dt>INFORMATION</dt>
+                    <dd>{this.state.profiles.description}</dd>
+                    <dt>COMPANY NAME</dt>
+                    <dd>{this.state.profiles.company_name}</dd>
+                    <dt>ADDRESS</dt>
+                    <dd>{this.state.profiles.address}</dd>
+                  </dl>
                 </div>
-                <br/>
-                <Link to={'orderhistoryseller'} id="history">VIEW ORDER HISTORY</Link>                 
-                </div> 
               </div>
-         </div>
+              <Link to={'orderhistoryseller'} id="history">VIEW ORDER HISTORY</Link>                 
+            </div> 
+          </div>
+        </div>
       )
     } else {
       return null;
