9c5345c3f50b4d0914e1815f6fb7c544e08413d3
babestvl <babestvl@gmail.com>
2017-11-05 18:51:23 +0700

    Add admin route

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ import ItemPurchaseTable from './pages/ItemPurchaseTable'
 import Register from './pages/Register'
 import RegisterSeller from './pages/RegisterSeller'
 import RegisterBuyer from './pages/RegisterBuyer'
+import AdminLogin from './pages/AdminLogin'
 import Logout from './pages/Logout'
 
 const history = createHistory()
@@ -65,6 +66,7 @@ ReactDOM.render(
           <Route path="/registerbuyer" component={RegisterBuyer}/>
           <Route exact path="/addproduct" component={AddProduct}/>
           <Route exact path="/editproduct" component={EditProduct}/>
+          <Route path="/admin" component={AdminLogin} />
           <Route path="/logout" component={Logout} />
         </App>
       </Switch>
